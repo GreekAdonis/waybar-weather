842c0b5e9fb7d3fbf60daedd3a4e0e2009484f12
Winni Neessen <wn@neessen.dev>
2025-11-06 23:21:57 +0100

    Mapped WMO weather codes and enhanced weather output
    - Added a map for WMO weather codes with descriptions for better readability
    - Updated weather output format to include weather condition descriptions and temperature
    - Improved handling of weather data presentation with detailed condition mapping
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

--- main.go
+++ main.go
@@ -12,6 +12,58 @@ import (
 	"github.com/maltegrosse/go-geoclue2"
 )
 
+/*
+WMO Weather interpretation codes (WW)
+Code	Description
+0	Clear sky
+1, 2, 3	Mainly clear, partly cloudy, and overcast
+45, 48	Fog and depositing rime fog
+51, 53, 55	Drizzle: Light, moderate, and dense intensity
+56, 57	Freezing Drizzle: Light and dense intensity
+61, 63, 65	Rain: Slight, moderate and heavy intensity
+66, 67	Freezing Rain: Light and heavy intensity
+71, 73, 75	Snow fall: Slight, moderate, and heavy intensity
+77	Snow grains
+80, 81, 82	Rain showers: Slight, moderate, and violent
+85, 86	Snow showers slight and heavy
+95 *	Thunderstorm: Slight or moderate
+96, 99 *	Thunderstorm with slight and heavy hail
+
+(*) Thunderstorm forecast with hail is only available in Central Europe
+*/
+
+// WMOWeatherCodes maps WMO weather code integers to their descriptions
+var WMOWeatherCodes = map[float64]string{
+	0:  "Clear sky",
+	1:  "Mainly clear",
+	2:  "Partly cloudy",
+	3:  "Overcast",
+	45: "Fog",
+	48: "Depositing rime fog",
+	51: "Light drizzle",
+	53: "Moderate drizzle",
+	55: "Dense drizzle",
+	56: "Light freezing drizzle",
+	57: "Dense freezing drizzle",
+	61: "Slight rain",
+	63: "Moderate rain",
+	65: "Heavy rain",
+	66: "Light freezing rain",
+	67: "Heavy freezing rain",
+	71: "Slight snow fall",
+	73: "Moderate snow fall",
+	75: "Heavy snow fall",
+	77: "Snow grains",
+	80: "Slight rain showers",
+	81: "Moderate rain showers",
+	82: "Violent rain showers",
+	85: "Slight snow showers",
+	86: "Heavy snow showers",
+	95: "Thunderstorm",
+	96: "Thunderstorm with slight hail",
+	99: "Thunderstorm with heavy hail",
+}
+
 var ErrLocationNotAccurate = errors.New("location service is not accurate enough")
 
 func main() {
@@ -47,7 +99,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Temperature in %s: %.1f°C\n", address.City, current.Temperature)
+	fmt.Printf("Weather conditions in %s: %s with a temperature of %.1f°C\n", address.City,
+		WMOWeatherCodes[current.WeatherCode], current.Temperature)
 }
 
 func getLocation() (float64, float64, error) {
